213af3361e5aa94be3d9573fc3b1b7966654658e
Cristina Gonzalez <cgonzalezescallon@gmail.com>
2019-01-24 10:29:37 +0100

    fin quete SortingHat

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Result from './Containers/Result';
+import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+import reducer from './reducer';
+import { createStore } from 'redux'
+
+
+const store = createStore(reducer);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <Result />
+    </Provider>,
+    document.getElementById('root')
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
